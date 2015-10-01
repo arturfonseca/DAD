63e95cc91562d17f6605a3612df6a3c7be826f62
Rui Botelho <botelhorui@tecnico.ulisboa.pt>
2015-10-01 23:21:56 +0100

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,120 @@
+﻿// Made by Rui, for course's lab
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+
+delegate void ThrWork();
+
+class ThrPool
+{
+
+    private Queue<ThrWork> tasks;
+    private List<Thread> threads;
+    private Semaphore sem;
+
+    public ThrPool(int thrNum, int bufSize)
+    {
+        sem = new Semaphore(0, 10);
+        tasks = new Queue<ThrWork>(bufSize);
+        threads = new List<Thread>();
+        for (int i = 0; i < thrNum; i++)
+        {
+            Thread t = new Thread(new ThreadStart(this.work));
+            t.Name = String.Format("Worker:{0}", i);
+            t.Start();
+            threads.Add(t);
+        }
+    }
+
+    public void work()
+    {
+        ThrWork w;
+        while (true)
+        {
+            sem.WaitOne();
+            lock (tasks)
+            {                
+                w = tasks.Dequeue();
+            
+}
+            w(); // work here so other threads can keep going
+        }
+    }
+
+    public void AssyncInvoke(ThrWork action)
+    {
+        lock (tasks)
+        {
+            tasks.Enqueue(action);            
+        }
+        sem.Release();
+
+
+    }
+
+    internal void stop()
+    {
+        foreach(Thread t in threads)
+        {
+            t.Abort();
+            t.Join();
+        }
+    }
+}
+
+
+class A
+{
+    private int _id;
+
+    public A(int id)
+    {
+        _id = id;
+    }
+
+    public void DoWorkA()
+    {
+        Console.WriteLine("A-{0}", _id);
+    }
+}
+
+
+class B
+{
+    private int _id;
+
+    public B(int id)
+    {
+        _id = id;
+    }
+
+    public void DoWorkB()
+    {
+        Console.WriteLine("B-{0}", _id);
+    }
+}
+
+
+class Test
+{
+    public static void Main()
+    {
+        ThrPool tpool = new ThrPool(5, 10);
+        ThrWork work = null;
+        for (int i = 0; i < 5; i++)
+        {
+            A a = new A(i);
+            tpool.AssyncInvoke(new ThrWork(a.DoWorkA));
+            B b = new B(i);
+            tpool.AssyncInvoke(new ThrWork(b.DoWorkB));
+        }
+        Thread.Sleep(100);
+        Console.WriteLine("press enter to stop");
+        Console.ReadLine();
+        tpool.stop();
+        Console.WriteLine("Thread Pool stopped successfully!\npress any key");
+        Console.ReadLine(); 
+
+    }
+}
