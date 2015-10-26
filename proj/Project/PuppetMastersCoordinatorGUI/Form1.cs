2120cd95160d3109e39cc910ae54efa7e656daaa
arturfonseca <artur.fonseca@live.com>
2015-10-26 15:39:32 +0000

    ...

--- proj/Project/PuppetMastersCoordinatorGUI/Form1.cs
+++ proj/Project/PuppetMastersCoordinatorGUI/Form1.cs
@@ -24,13 +24,14 @@ namespace PuppetMastersCoordinatorGUI
         Dictionary<String, Broker> site_brokers;
         Dictionary<String, List<Publisher>> site_publishers;
         Dictionary<String, List<Subscriber>> site_subscribers;
-        List<Broker> all_brokers;
-        List<Publisher> all_publishers;
-        List<Subscriber> all_subscribers;
+        Dictionary<String, Broker> all_brokers;
+        Dictionary<String, Publisher> all_publishers;
+        Dictionary<String, Subscriber> all_subscribers;
         //Vars
         String site_root;
         RoutingPolicy rout;
         OrderingPolicy ord;
+        String log = "light";
 
         public Form1()
         {
@@ -79,9 +80,9 @@ namespace PuppetMastersCoordinatorGUI
             site_brokers = new Dictionary<String, Broker>();
             site_publishers = new Dictionary<String, List<Publisher>>();
             site_subscribers = new Dictionary<String, List<Subscriber>>();
-            all_brokers = new List<Broker>();
-            all_publishers = new List<Publisher>();
-            all_subscribers = new List<Subscriber>();
+            all_brokers = new Dictionary<String, Broker>();
+            all_publishers = new Dictionary<String, Publisher>();
+            all_subscribers = new Dictionary<String, Subscriber>();
             rout = RoutingPolicy.flooding;
             ord = OrderingPolicy.fifo;
             getPMs();
@@ -97,6 +98,12 @@ namespace PuppetMastersCoordinatorGUI
                 {
                     if (keywords[1] == "filter")
                         rout = RoutingPolicy.filter;
+
+
+                }
+                else if (keywords[0] == "LoggingLevel" && keywords.Length >= 2)
+                {
+                    log = keywords[1];
 
 
                 }
@@ -133,20 +140,20 @@ namespace PuppetMastersCoordinatorGUI
                     {
                         case "publisher":
                             Publisher p = pms[ip].createPublisher(name, keywords[5], Int32.Parse(port));
-                            all_publishers.Add(p);
+                            all_publishers.Add(keywords[1], p);
                             if (!site_publishers.ContainsKey(keywords[5]))
                                 site_publishers.Add(keywords[5], new List<Publisher>());
                             site_publishers[keywords[5]].Add(p);
                             break;
                         case "broker":
                             Broker b = pms[ip].createBroker(name, keywords[5], Int32.Parse(port));
-                            all_brokers.Add(b);
+                            all_brokers.Add(keywords[1], b);
                             site_brokers.Add(keywords[5], b);
                             site_site.Add(keywords[5], new Site() { name = keywords[5], brokers = new List<Broker>() { b } });
                             break;
                         case "subscriber":
                             Subscriber s = pms[ip].createSubscriber(name, keywords[5], Int32.Parse(port));
-                            all_subscribers.Add(s);
+                            all_subscribers.Add(keywords[1], s);
                             if (!site_subscribers.ContainsKey(keywords[5]))
                                 site_subscribers.Add(keywords[5], new List<Subscriber>());
                             site_subscribers[keywords[5]].Add(s);
@@ -161,11 +168,12 @@ namespace PuppetMastersCoordinatorGUI
                 else
                     MessageBox.Show("Error parsing config.file!");
             }
-            foreach (Broker b in all_brokers)
+            foreach (KeyValuePair<string, Broker> entry in all_brokers)
             {
-                b.setRoutingPolicy(rout);
-                b.setOrderingPolicy(ord);
+                entry.Value.setRoutingPolicy(rout);
+                entry.Value.setOrderingPolicy(ord);
             }
+
             site_brokers[site_root].setIsRoot();
 
             //Set publishers brokers
@@ -220,13 +228,14 @@ namespace PuppetMastersCoordinatorGUI
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            /*
             // foreach (KeyValuePair<string, PuppetMaster> entry in pms)
             //   MessageBox.Show("PM @ " + entry.Key + " " + entry.Value.status());
-            foreach(Subscriber s in site_subscribers["site0"])
+            foreach (Subscriber s in site_subscribers["site0"])
                 s.subscribe("/tempo/lisboa");
-
-            foreach (Publisher p in site_publishers["site0"])
+            foreach (Subscriber s in site_subscribers["site2"])
+                s.subscribe("/tempo/lisboa");
+            foreach (Publisher p in site_publishers["site5"])
             {
                 p.publish("/tempo/lisboa", "chove");
                 p.publish("/tempo/porto", "neve");
@@ -237,15 +246,77 @@ namespace PuppetMastersCoordinatorGUI
                 s.unsubscribe("/tempo/lisboa");
                 s.subscribe("/tempo/*");
             }
-                
-            foreach (Publisher p in site_publishers["site0"])
+            foreach (Subscriber s in site_subscribers["site2"])
+            {
+                s.unsubscribe("/tempo/lisboa");
+                s.subscribe("/tempo/*");
+            }
+
+            foreach (Publisher p in site_publishers["site5"])
             {
                 p.publish("/tempo/lisboa", "chove");
                 p.publish("/tempo/porto", "neve");
             }
 
+    */
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not implemented yet");
+            //readInput(file);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            readInput(textBox1.Lines);
 
 
         }
+        public void readInput(String[] lines)
+        {
+            foreach (String line in lines)
+            {
+                string[] keywords = line.Split(' ');
+                if (keywords[0] == "Status" && keywords.Length >= 1)
+                {
+
+                }
+                else if (keywords[0] == "Crash" && keywords.Length >= 2)
+                {
+
+                }
+                else if (keywords[0] == "Freeze" && keywords.Length >= 2)
+                {
+
+                }
+                else if (keywords[0] == "Unfreeze" && keywords.Length >= 2)
+                {
+
+                }
+                else if (keywords[0] == "Wait" && keywords.Length >= 2)
+                {
+
+                }
+                else if (keywords.Length >= 4 && keywords[0] == "Subscriber" && keywords[2] == "Subscribe")
+                {
+
+                }
+                else if (keywords.Length >= 4 && keywords[0] == "Subscriber" && keywords[2] == "Unsubscribe")
+                {
+
+                }
+                else if (keywords.Length >= 8 && keywords[0] == "Publisher" && keywords[0] == "Publish")
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("Error on reading input");
+                }
+
+            }
+        }
+
     }
-}
